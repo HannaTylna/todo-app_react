914ceb40504e06f8ea21b3d1cb64e8e5586cd9d7
HannaTylna <hanna.tylna@gmail.com>
2022-11-21 10:10:36 +0100

    fix login frontend

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,31 +1,106 @@
-﻿import React, { Component } from 'react';
+﻿import React, { Component } from "react";
+import { useNavigate } from "react-router-dom";
 
 export class Login extends Component {
-    static displayName = Login.name;
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        let customerObj = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        let payload = {
+            method: 'POST',
+            headers: {
+                //"access-control-allow-origin": "*",
+                'accept': 'text/plain',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(customerObj)
+
+        }
+        fetch('https://localhost:7007/api/v1/authenticate/login', payload)
+            .then((response) => response.json())
+            .then((result) => {
+                //setUserSession(email, token) {
+                //    sessionStorage.setItem('email', email);
+                //    sessionStorage.setItem('token', token);
+                //};
+                //let responseJson = result;
+                console.log(result)
+                let responseJson = result;
+                let token = result.accessToken;
+                localStorage.setItem('todo_dotnet', token);
+                if (responseJson) {
+                    window.location.href = "/home";
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+
+        //getToken() {
+        //    const token = sessionStorage.getItem('token');
+        //    if (token) return token;
+        //    else return null;
+        //},
+
+        //setUserSession(userName, token, userId, usersRole) {
+        //    sessionStorage.setItem('userName', userName);
+        //    sessionStorage.setItem('token', token);
+        //    sessionStorage.setItem('userId', userId);
+        //    sessionStorage.setItem('usersRole', usersRole);
+        //}
+        //postData('api/Customer/Create', customerObj).then((result) => {
+        //    let responseJson = result;
+        //    if (responseJson) {
+        //        history.push('/login');
+        //    }
+        //});
+    }
 
     render() {
         return (
-            <div className="p-5 border-style" >
-                <div className="text-center">
-                    <h3 className="text-uppercase">Login</h3>
-                </div>
+            <div className="row">
+                <div className="col-md-4">
+                    <form onSubmit={this.onSubmit} className="p-5 border-style">
+                        <h3 className="text-center">Login</h3>
+                        <div className="form-group">
+                            <label className="control-label">Email: </label>
+                            <input className="form-control" type="text" name="email" value={this.state.email} onChange={this.onChange}></input>
+                        </div>
 
-                <div className="row">
-                    <div className="input-field col s6">
-                        <input value="" id="username" type="text" className="validate" placeholder="Enter your username" />
-                        <label className="active" htmlFor="username">Username</label>
-                    </div>
+                        <div className="form-group">
+                            <label className="control-label">Password:  </label>
+                            <input className="form-control" type="password" name="password" value={this.state.password} onChange={this.onChange}></input>
+                        </div>
+
+                        <div className="form-group text-center mt-5">
+                            <input type="submit" value="Login" className="btn waves-effect waves-light"></input>
+                        </div>
+
+                    </form>
+
                 </div>
-                <div className="row">
-                    <div className="input-field col s6">
-                        <input value="" id="password" type="text" className="validate" placeholder="Enter password" />
-                        <label className="active" htmlFor="password">Password</label>
-                    </div>
-                </div>
-                <div className="text-center">
-                    <button className="btn waves-effect waves-light" type="submit" name="action">Login</button>
-                </div>
-            </div >
-        );
+            </div>
+        )
     }
-} 
+
+}
+
